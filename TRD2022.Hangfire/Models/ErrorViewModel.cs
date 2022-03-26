d9932689321b1c2bdc164c4b032fc54776230dda
Andrew Moraes <andrewmoraesds@gmail.com>
2022-03-26 08:53:10 -0300

    initial commit

---
+++ TRD2022.Hangfire/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TRD2022.Hangfire.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
