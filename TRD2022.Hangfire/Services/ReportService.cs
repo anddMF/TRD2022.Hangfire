a653791d9621f792ea206f493dba7ca574e05690
Andrew Moraes <andrewmoraesds@gmail.com>
2022-03-26 18:26:25 -0300

    funcao de enviar posicoes ao DB

--- TRD2022.Hangfire/Services/ReportService.cs
+++ TRD2022.Hangfire/Services/ReportService.cs
@@ -24,14 +24,42 @@ namespace TRD2022.Hangfire.Services
             try
             {
                 _fileService = new FileCommunication(AppSettings.TRDFolder + "\\REPORTS");
-                var result = _fileService.GetDataFromCsv($"REPORTS-{DateTime.Now.AddDays(-1).ToString("yyyyMMdd")}").Select(x => TransformLineIntoPosition(x)).ToList();
+                List<Position> positions = _fileService.GetDataFromCsv($"REPORTS-{DateTime.Now.AddDays(-1).ToString("yyyyMMdd")}").Select(x => TransformLineIntoPosition(x)).ToList();
+
+                SendToDB(positions);
             }
             catch (Exception ex)
             {
                 throw ex;
             }
+        }
 
+        private void SendToDB(List<Position> positions)
+        {
+            _dbService = new DBCommunication(AppSettings.ConnString);
+            // TODO: find a bulk copy for mysql
+            foreach(Position pos in positions)
+            {
+                Dictionary<string, object> param = PositionIntoDictionary(pos);
+                var res = _dbService.ExecuteProc("TRD2022_InsertPosition", param);
+            }
+        }
 
+        private Dictionary<string, object> PositionIntoDictionary(Position pos)
+        {
+            Dictionary<string, object> res = new Dictionary<string, object>();
+
+            res.Add("symbol", pos.Symbol);
+            res.Add("type", pos.OperationType);
+            res.Add("dtOperation", pos.DtOperation);
+            res.Add("initialPrice", pos.InitialPrice);
+            res.Add("finalPrice", pos.LastPrice);
+            res.Add("initialTotal", pos.InitialValue);
+            res.Add("finalTotal", pos.LastValue);
+            res.Add("valorization", pos.Valorization);
+            res.Add("quantity", pos.Quantity);
+
+            return res;
         }
 
         private Position TransformLineIntoPosition(string line)
